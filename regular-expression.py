54c352295f31fb960657d80934f731a856ee21cb
flakesns <flakesns@gmail.com>
2016-05-19 15:37:00 +0800

    Regular expression
    Hi, these are few example on my daily tasks using the regular expression in Python. The lists will be updated from time to time.

---
+++ regular-expression.py
@@ -0,0 +1,7 @@
+import re
+text= "UPDATE `my_table` SET `col1` = '' WHERE `col_id` = '123456'"
+searchObj = re.search( r"`col_id` = '\d+'",text, re.M|re.I)
+if searchObj:
+    print "Result : ", searchObj.group()
+else:
+    print "Nothing found!!"
